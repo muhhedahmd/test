bfa412f047a31877825f2e9ba5912c8f03fcd9b5
Abdalrhman <body362222@gmail.com>
2026-06-23 18:06:16 +0300

    feat:msg

--- Test/models/Accounting.py
+++ Test/models/Accounting.py
@@ -1,5 +1,5 @@
 from odoo import models , fields
 class Accounting(models.Model):
     _inherit = "account.move"
-    x_custom_field = fields.Char(string="Name")
+    custom_type = fields.Char(string="Name")
 
