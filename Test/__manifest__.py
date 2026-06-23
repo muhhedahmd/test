d85840a18e07e9a425edc0436a83ee9199f77a20
Abdalrhman <body362222@gmail.com>
2026-06-23 14:53:34 +0300

    feat:msg

---
+++ Test/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Test',
+    'license': 'LGPL-3',
+    'author': 'Abdalrhman Salah',
+    'category': '',
+    'version': '19.0.1.0.0',
+    'depends': ['base','account'],
+    'data': [
+        "views/accounting.xml"
+    ],
+    'application': True,
+}
