7df7bf8509fc2f34dd842409b6b2ef4eb8eca08a
Abdalrhman <body362222@gmail.com>
2026-06-27 23:00:19 +0300

    dd

--- estate/models/estate.py
+++ estate/models/estate.py
@@ -4,9 +4,11 @@ class Estate (models.Model):
     _name = 'estate'
     _description = 'Estate'
 
-    name =  fields.Char(required = True)
+    name = fields.Char(required=True)
+    datatime = fields.Datetime(default=fields.Datetime.now)
     date = fields.Date(default=fields.Date.today)
     expected_price = fields.Float(required = True)
     best_price = fields.Float(readonly=True) 
     test= fields.Char(readonly= True) 
     active = fields.Boolean(default= False )
+    
