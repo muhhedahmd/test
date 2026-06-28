e2ff0313360b3dbd934ed12d02cbe1e94d376d60
Abdalrhman <body362222@gmail.com>
2026-06-28 17:51:51 +0300

    s

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,5 +1,5 @@
 {'name':'estate',
-'depends':['base'] , 
+'depends':['base', 'account'] , 
 'data':['security/ir.model.access.csv' , 'views/estate_views.xml', 'views/estate_type_view.xml', 'views/estate_log_views.xml'],
 'application' :True,
 }
--- estate/models/__init__.py
+++ estate/models/__init__.py
@@ -1,5 +1,5 @@
 from . import estate
 from . import estate_type
 from . import estate_log
-from . import base_log
+from . import base_logger
 from . import res_users
