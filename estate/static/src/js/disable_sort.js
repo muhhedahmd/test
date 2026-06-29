34c40eea889d60251d13654aac37ba3c0dbb155b
Abdalrhman <body362222@gmail.com>
2026-06-29 23:31:17 +0300

    sort

---
+++ estate/static/src/js/disable_sort.js
@@ -0,0 +1,13 @@
+/*
+import { ListRenderer } from "@web/views/list/list_renderer";
+import { patch } from "@web/core/utils/patch";
+
+patch(ListRenderer.prototype, {
+    onClickSortColumn(column) {
+        if (this.props.classNames && this.props.classNames.includes('disable_sort')) {
+            return;
+        }
+        super.onClickSortColumn(column);
+    },
+});
+*/
