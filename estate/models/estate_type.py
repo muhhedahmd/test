a590e3c2207d37432f0bdb574200bc9121d30c81
Abdalrhman <body362222@gmail.com>
2026-06-30 16:48:29 +0300

    feat: add EstateType model with initial fields and test method

--- estate/models/estate_type.py
+++ estate/models/estate_type.py
@@ -3,8 +3,12 @@ from odoo import models, fields
 class EstateType (models.Model):
     _name = 'estate.type'
     _description = 'Estate Type'
+    _order = "id desc"
 
     name = fields.Char(required=True)
     code = fields.Char(required=True)
     active = fields.Boolean(default=True)
     estateType = fields.One2many('estate', 'Test2', string='Estate Type')
+    def action_test(self):
+        properties = self.env["estate"].search([])
+        print(properties)
