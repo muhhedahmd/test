6bfda9be35f5cef6331feb63558963885ac1fc58
Abdalrhman <body362222@gmail.com>
2026-07-01 15:05:35 +0300

    feat: add advanced_web_domain_widget stub module to satisfy simplify_access_management dependencies

---
+++ advanced_web_domain_widget/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Advanced Web Domain Widget (Stub)',
+    'version': '19.0.1.0.0',
+    'category': 'Hidden',
+    'summary': 'Stub for Advanced Web Domain Widget dependency',
+    'depends': ['web'],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ advanced_web_domain_widget/models/domain_prepare.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+def prepare_domain_v2(dom_tuple):
+    """
+    Stub for prepare_domain_v2.
+    Converts or returns domain tuples.
+    """
+    return [dom_tuple]
