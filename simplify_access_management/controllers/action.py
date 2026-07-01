024effec6291afd50aa80e4e90c3c52bb59ecde9
Abdalrhman <body362222@gmail.com>
2026-07-01 16:08:43 +0300

    fix: support both /web and /odoo backend routes in home controller override

--- simplify_access_management/controllers/action.py
+++ simplify_access_management/controllers/action.py
@@ -38,7 +38,7 @@ class Action(Action):
 
 class Home(Home):
 
-    @http.route('/web', type='http', auth="none")
+    @http.route(['/web', '/odoo'], type='http', auth="none")
     def web_client(self, s_action=None, **kw):
         ensure_db()
         # request.env['ir.ui.view'].flush_recordset()
@@ -57,7 +57,8 @@ class Home(Home):
                 if cids:
                     access_management = request.env['access.management'].sudo().search([('active','=',True),('company_ids','in',int(cids)),('disable_debug_mode','=',True),('user_ids','in',user.id)],limit=1)
                     if access_management.id:
-                        return request.redirect('/web?debug=0')
+                        redirect_path = '/odoo' if request.httprequest.path.startswith('/odoo') else '/web'
+                        return request.redirect(f'{redirect_path}?debug=0')
                         # request.session.debug = '0'
 
         return super(Home, self).web_client(s_action=s_action, **kw)
